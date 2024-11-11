618afe20487d6b9b38873e0f65512b96f1b13beb
MansiS117 <msongirkar@bestpeers.com>
2024-11-11 15:41:45 +0530

    formatted the code

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100 , blank=True)
+    title = models.CharField(max_length=100, blank=True)
     code = models.TextField()
     linenos = models.BooleanField(default=False)
 
     class Meta:
-        ordering  = ['created']
+        ordering = ["created"]
